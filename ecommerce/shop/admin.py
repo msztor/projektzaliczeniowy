c135757c4d1df12d111e5833c247fff95d1c95ec
michal <msztorch@st.swps.edu.pl>
2025-01-17 17:02:01 +0100

    modyfikacja modeli dodanie do panelu administratora

--- ecommerce/shop/admin.py
+++ ecommerce/shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Customer, Category, Order, OrderProducts, Employee, Product
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id','name', 'price', 'description']
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(Employee)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(OrderProducts)
+admin.site.register(Product, ProductAdmin)
--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
 # Create your models here.
+
+# propozycje modeli:
+#  Employee, Product, Category, Customer, Order
+
+class RoleType(models.TextChoices):
+    Sales = 'Sales'
+    Marketing = 'Marketing'
+
+class Employee(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    role = models.CharField(max_length=50, choices=RoleType.choices)
+    age = models.PositiveIntegerField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+class Product(models.Model):
+    price =models.DecimalField(decimal_places=2, max_digits=7)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Customer(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+class Order(models.Model):
+    number = models.IntegerField(primary_key=True)
+    date_created =models.DateField(auto_now=True)
+    Customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class OrderProducts(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product =models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(decimal_places=2, max_digits=7)
+
+    class Meta:
+        unique_together = (('order', 'product'),)
+
+   
+
